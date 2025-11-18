47a56a5c63679146830e870f5c3bb28ee78bdae8
hanssav <handihandi40@gmail.com>
2025-11-18 14:07:29 +0700

    animate/footer
    Signed-off-by: hanssav <handihandi40@gmail.com>

--- src/components/container/layout-wrapper.tsx
+++ src/components/container/layout-wrapper.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './navbar';
 import { cn } from '@/lib/utils';
+import Footer from './footer';
 
 type LayoutWrapperProps = {
   className?: string;
@@ -14,11 +15,7 @@ const LayoutWrapper: React.FC<LayoutWrapperProps> = ({
     <div className={cn('bg-background text-foreground w-full', className)}>
       <Navbar />
       {children}
-      <footer className='flex-center border-t border-neutral-800 px-2 py-5 lg:py-[25px]'>
-        <p className='desc md:text-md-regular text-xs-regular'>
-          © 2025 Edwin Anderson. All rights reserved.
-        </p>
-      </footer>
+      <Footer />
     </div>
   );
 };
